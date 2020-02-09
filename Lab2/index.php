ddd3b42c3181cf0d772db7b88ce9b0ff7031a4b8
gelamari <angela.gavin@ryerson.ca>
2020-02-08 19:41:54 -0500

    yup

--- Lab2/index.php
+++ Lab2/index.php
@@ -10,7 +10,7 @@ $type = $_POST['type'];
 $ccs = implode(', ', $_POST['ccs']);
 
 
-$artwork = array("Title"=>$title,"Description"=>$description, "Genre"=>$genre, "Subject"=>$subject, "Year"=>$year, "Museum"=>$museum, "Type"=>$type, "Creative sCommons Specifications"=>$ccs);
+$artwork = array("Title"=>$title,"Description"=>$description, "Genre"=>$genre, "Subject"=>$subject, "Year"=>$year, "Museum"=>$museum, "Type"=>$type, "Creative Commons Specifications"=>$ccs);
 
 $i=0;
 foreach ($artwork as $key => $value) {
