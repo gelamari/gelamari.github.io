153f5aa516cb4d5aa5be1bf9167253a618451fad
gelamari <angela.gavin@ryerson.ca>
2020-02-08 19:39:09 -0500

    php

---
+++ Lab2/index.php
@@ -0,0 +1,22 @@
+<?php 
+if (isset($_POST['submit'])){
+$title = $_POST['title']; 
+$description = $_POST['description'];
+$genre = $_POST['genre'];
+$subject = $_POST['subject'];
+$year = $_POST['year'];
+$museum = $_POST['museum'];
+$type = $_POST['type'];
+$ccs = implode(', ', $_POST['ccs']);
+
+
+$artwork = array("Title"=>$title,"Description"=>$description, "Genre"=>$genre, "Subject"=>$subject, "Year"=>$year, "Museum"=>$museum, "Type"=>$type, "Creative sCommons Specifications"=>$ccs);
+
+$i=0;
+foreach ($artwork as $key => $value) {
+	# code...
+	echo "<strong>" .$key. "</strong>". ": " . $value . "<br>";
+}
+
+}
+?>
